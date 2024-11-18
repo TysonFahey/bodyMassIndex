5310a01ddaacbf11ae9b0af5a89bd6c2b73d95bd
TysonFahey <103978328+TysonFahey@users.noreply.github.com>
2024-11-18 01:13:48 -0600

    first commit

---
+++ body-mass/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BodyMassIndexForm from "./components/BodyMassIndexForm.vue"; // import BodyMassIndexForm 
+
+function calculate() {
+    const bmi = weight / (height * height)
+}
+/// body mass is found by dividing weight by the height multiplied by itself
+</script>
+
+<template>
+  <h1>Body Mass Index Calculator</h1> 
+
+  <BodyMassIndexForm></BodyMassIndexForm> <!-- Display BodyMassIndexForm.vue-->
+
+  <h2 v-on:stats-entered="calculate()">BMI is: {{bmi}}</h2> <!-- when the calulate button is pressed, call calculate function, display bmi -->
+
+</template>
+
+<style scoped></style>
---
+++ body-mass/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+const emit = defineEmits("stats-entered");
+</script>
+
+<template>
+  <div>
+    <label for="height">Height in meters </label> <!-- height input label-->
+    <input id="height" v-model="stats" /><!-- height input -->
+  </div>
+  <div>
+    <label for="weight">Weight in kilograms </label> <!-- weight input label-->
+    <input id="weight" v-model="stats" /> <!-- weight input label-->
+  </div>
+
+    <button v-on:click="emit('stats-entered',[weight, height])">Calculate</button> <!-- When button is pressed, emit so the parent can read input-->
+
+</template>
+
+<style scoped>
+</style>
